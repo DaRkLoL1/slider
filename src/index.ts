bd8b3c101748b860f3debc82a7ba7e3d18b0e829
Igor Frolov <darklol_777@mail.ru>
2019-09-30 00:24:21 +0300

    подключал тесты

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {Car} from './Car';
+import './index.scss';
+
+console.log('hi');
+$('body').text('a112321');
+const car: Car = new Car();
+car.go('vroom');
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index',
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+           'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    hot: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      filename: 'index.html',
+      chunks: ['index']
+    }),
+  ],
+};
