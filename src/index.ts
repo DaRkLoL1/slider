b6d09e37fde0c1d2419fd77cd882c0adb08d9095
Igor Frolov <darklol_777@mail.ru>
2019-09-30 23:06:21 +0300

    создать ручку и получить value

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export class MainModel { 
+
+  constructor(private min : number = 0, private max: number = 100) {};
+
+  getMinMax(): number[] {
+    return [this.min, this.max];
+  };
+  
+}
+
+export class ModelHandle {
+  constructor(private value : number = 1) {}
+
+  getValue() : number {
+    return 50;
+  }
+}
--- test/index-test.ts
+++ test/index-test.ts
@@ -1,20 +1,29 @@
+import {MainModel} from '../src/index';
+import {ModelHandle} from '../src/index';
 
-class MainModel { 
-
-  constructor(private min : number, private max: number) {
-
-  }
-  getMinMax(): number[] {
-    return [this.min, this.max];
-  }
-}
 
 
 describe('Model', function () {
 
   it('создать конструктор для присвоения min max и вернуть их', function () {
+    let model : MainModel = new MainModel();
+    let minMax : number[] = model.getMinMax()
+    expect(minMax).toEqual([0, 100]);
+  });
+
+  it('создать конструктор для присвоения min max и вернуть их', function () {
     let model : MainModel = new MainModel(0, 100);
     let minMax : number[] = model.getMinMax()
     expect(minMax).toEqual([0, 100]);
+  });
+
+});
+
+describe('model handle', function () {
+
+  it('создать ручку и получить значение', function (){
+    let handle = new ModelHandle();
+    let value = handle.getValue()
+    expect(value).toEqual(50);
   })
 });
