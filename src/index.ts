f0d5dbe4837bb1a78e4d50591179dd93cb871864
Igor Frolov <darklol_777@mail.ru>
2019-10-11 19:51:58 +0300

    настроил конфигурацию и инпут

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import './index.scss';
-import { deflateRaw } from 'zlib';
-import { numberTypeAnnotation } from 'babel-types';
 
 interface SubjectView {
   addObserverView(o : ObserverView) : void;
@@ -137,7 +135,7 @@ export class View implements ObserverView, SubjectView {
     this.position = obj.position;
 
     let width : number | undefined;
-
+     
     if(this.position === 'vertical') {
       $(this.item).html('<div class="slider slider_vertical"><div class="slider__field slider__field_vertical"></div></div>');
       width  = $(this.item).height();
@@ -162,10 +160,10 @@ export class View implements ObserverView, SubjectView {
 
       if(obj.tooltip) {
         if(this.position === 'vertical') {
-          $(this.item).find('.slider_vertical').prepend($('<div class="slider__tooltip slider__tooltip_vertical"></div>'));
+          $(this.item).find('.slider__field_vertical').prepend($('<div class="slider__tooltip slider__tooltip_vertical"></div>'));
           this.tooltip = new ViewTooltip(this.item.find('.slider__tooltip_vertical'));
         } else {
-          $(this.item).find('.slider').prepend($('<div class="slider__tooltip"></div>'));
+          $(this.item).find('.slider__field').prepend($('<div class="slider__tooltip"></div>'));
           this.tooltip = new ViewTooltip(this.item.find('.slider__tooltip'));
         }
         
@@ -474,8 +472,8 @@ class Prezenter implements ObserverView, ObserverModel {
   let def = {
     min: 0,
     max: 100,
-    step: 10,
-    value: 50, 
+    step: 25,
+    value: 0, 
     tooltip: false,
     interval: false,
     position: 'horisontal',
@@ -528,11 +526,121 @@ class Prezenter implements ObserverView, ObserverModel {
 })( jQuery );
 
 
-(<any>$('.rooot1')).myPlugin({interval : true, slide : function (num : number) {
-  $('.root__input').val(num);
-}});
+$('.configure__button').on('click', function (event) {
+  let parent = $(event.currentTarget).parent();
 
-$('.root__input').on('input', function (event) {
+  interface Obj {
+    min ? : number;
+    max ? : number;
+    step ? : number;
+    interval ? : boolean;
+    tooltip ? : boolean;
+    position ? : string;
+    value ? : number;
+    slide : (num : number) => void;
+  };
+  
+
+  let obj : Obj = {
+    slide(num) {
+      parent.find('.configure__input').val(num);
+    }
+  };
+
+  let value = parent.find('.configure__input').val();
+
+  if(value && typeof value === 'string') {
+    obj.value = Number.parseInt(value);
+  }
+
+  let min = parent.find('.configure__min').val();
+
+  if(min && typeof min === 'string') {
+    obj.min = Number.parseInt(min);
+  }
+
+  let max = parent.find('.configure__max').val();
+
+  if(max && typeof max === 'string') {
+    obj.max = Number.parseInt(max);
+  }
+
+  let step = parent.find('.configure__step').val();
+
+  if(step && typeof step === 'string') {
+    obj.step = Number.parseInt(step);
+  }
+
+  if(parent.find('.configure__tooltip').prop('checked')) {
+    obj.tooltip = true;
+  }
+
+  if(parent.find('.configure__interval').prop('checked')) {
+    obj.interval = true;
+  }
+
+  if(parent.find('.configure__position').prop('checked')) {
+    obj.position = 'vertical';
+  }
+  ( <any>$(parent.parent().children()[0]) ).myPlugin(obj);
+});
+
+
+
+(<any>$('.root-slider')).myPlugin({
+  slide : function (num : number) {
+    $('.root-input').val(num);
+  }
+});
+
+$('.root-input').val((<any>$('.root-slider')).myPlugin('value'));
+
+$('.root-input').on('change', function (event) {
   let target = $(event.currentTarget);
-  (<any>$('.rooot1')).myPlugin('value', target.val());
-})
+  (<any>$('.root-slider')).myPlugin('value', target.val());
+});
+
+
+
+(<any>$('.root-slider1')).myPlugin({
+  slide : function (num : number) {
+    $('.root-input1').val(num);
+  }
+});
+
+$('.root-input1').val((<any>$('.root-slider1')).myPlugin('value'));
+
+$('.root-input1').on('change', function (event) {
+  let target = $(event.currentTarget);
+  (<any>$('.root-slider1')).myPlugin('value', target.val());
+});
+
+
+
+(<any>$('.root-slider2')).myPlugin({
+  slide : function (num : number) {
+    $('.root-input2').val(num);
+  }
+});
+
+$('.root-input2').val((<any>$('.root-slider2')).myPlugin('value'));
+
+$('.root-input2').on('change', function (event) {
+  let target = $(event.currentTarget);
+  (<any>$('.root-slider2')).myPlugin('value', target.val());
+});
+
+
+
+(<any>$('.root-slider3')).myPlugin({
+  slide : function (num : number) {
+    $('.root-input3').val(num);
+  }
+});
+
+$('.root-input3').val((<any>$('.root-slider3')).myPlugin('value'));
+
+$('.root-input3').on('change', function (event) {
+  let target = $(event.currentTarget);
+  (<any>$('.root-slider3')).myPlugin('value', target.val());
+});
