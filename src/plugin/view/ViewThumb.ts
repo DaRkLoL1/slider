d8587aa4e72000f3699333cd2f0458c4c106d9e7
Igor Frolov <darklol_777@mail.ru>
2020-01-18 17:51:57 +0300

    Компактность методов #5

--- src/plugin/view/ViewThumb.ts
+++ src/plugin/view/ViewThumb.ts
@@ -4,6 +4,8 @@ class ViewThumb extends Observer {
   private symbol: string = '';
   private interval: number | undefined;
   private index: number;
+  private target: Element | undefined;
+  private width: number | undefined;
 
   constructor(private thumb: JQuery<HTMLElement>, index: number) {
     super();
@@ -13,16 +15,14 @@ class ViewThumb extends Observer {
   public installValue(value: number, interval: number): void {
     this.interval = interval;
 
-    let width: number | undefined;
-
     if (this.thumb.hasClass('slider__thumb_vertical')) {
-      width  = $(this.thumb).height();
+      this.width  = $(this.thumb).height();
     } else {
-      width  = $(this.thumb).width();
+      this.width  = $(this.thumb).width();
     }
 
-    if (typeof width === 'number') {
-      const left: string = value - width / 2 + 'px';
+    if (typeof this.width === 'number') {
+      const left: string = value - this.width / 2 + 'px';
 
       if (this.thumb.hasClass('slider__thumb_vertical')) {
         this.thumb.css('bottom', left);
@@ -30,89 +30,105 @@ class ViewThumb extends Observer {
         this.thumb.css('left', left);
       }
 
-      $(this.thumb).on('dragstart',  () => {
-        return false;
-      });
-
-      $(document).on('selectstart', () => {
-        return false;
-      });
-
-      $(this.thumb).on('mousedown', (event) => {
-        const target: Element = event.currentTarget;
-
-        const onMouseMove =  (event: MouseEvent) => {
-          let x: number;
-          if (this.thumb.hasClass('slider__thumb_vertical')) {
-            x =  event.clientY;
-          } else {
-            x =  event.clientX;
-          }
-
-          let thumbLeft: number;
-          if (typeof width === 'number' && typeof this.interval === 'number') {
-            if (this.thumb.hasClass('slider__thumb_vertical')) {
-              thumbLeft = target.getBoundingClientRect().top + width / 2;
-            } else {
-              thumbLeft = target.getBoundingClientRect().left + width / 2;
-            }
-
-            if ( x >= (thumbLeft + this.interval / 2 ) ) {
-              let symbol: string = '';
-              if (this.thumb.hasClass('slider__thumb_vertical')) {
-                symbol = '-';
-              } else {
-                symbol = '+';
-              }
-              let i = x - thumbLeft;
-              let counter = 0;
-              while (i > 0) {
-                if (i - this.interval >= 0) {
-                  i -= this.interval;
-                  counter += 1;
-                } else if (i - this.interval / 2 >= 0) {
-                  i -= this.interval / 2;
-                  counter += 1;
-                } else {
-                  i = 0;
-                }
-              }
-              this.notifySubscribers('moveThumb', {symbol, counter, index: this.index});
-            }
-
-            if (x <= (thumbLeft - this.interval / 2 )) {
-              let symbol: string = '';
-              if (this.thumb.hasClass('slider__thumb_vertical')) {
-                symbol = '+';
-              } else {
-                symbol = '-';
-              }
-              let i = thumbLeft - x;
-              let counter = 0;
-              while (i > 0) {
-                if (i - this.interval >= 0) {
-                  i -= this.interval;
-                  counter += 1;
-                } else if (i - this.interval / 2 >= 0) {
-                  i -= this.interval / 2;
-                  counter += 1;
-                } else {
-                  i = 0;
-                }
-              }
-              this.notifySubscribers('moveThumb', {symbol, counter, index: this.index});
-            }
-          }
-        };
-        document.addEventListener('mousemove', onMouseMove);
-
-        const onMouseUp = () => {
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-        };
-        document.addEventListener('mouseup', onMouseUp);
-      });
+      this.addHandlersEvents();
     }
+  }
+
+  public addHandlersEvents() {
+    this.thumb.on('dragstart', this.handleThumbDragStart);
+    $(document).on('selectstart', this.handleThumbDragStart);
+    this.thumb.on('mousedown', this.handleThumbMouseDown.bind(this));
+  }
+
+  public handleThumbDragStart() {
+    return false;
+  }
+
+  public handleThumbMouseDown(event: Event) {
+    this.target = (event.currentTarget as Element);
+    this.handleDocumentMouseMove = this.handleDocumentMouseMove.bind(this);
+    document.addEventListener('mousemove', this.handleDocumentMouseMove);
+    document.addEventListener('mouseup', this.handleDocumentMouseUp.bind(this));
+  }
+
+  public handleDocumentMouseMove(event: MouseEvent) {
+    let x: number;
+    if (this.thumb.hasClass('slider__thumb_vertical')) {
+      x =  event.clientY;
+    } else {
+      x =  event.clientX;
+    }
+
+    let thumbLeft: number;
+    if (typeof this.width === 'number' && typeof this.interval === 'number' && this.target) {
+      if (this.thumb.hasClass('slider__thumb_vertical')) {
+        thumbLeft = this.target.getBoundingClientRect().top + this.width / 2;
+      } else {
+        thumbLeft = this.target.getBoundingClientRect().left + this.width / 2;
+      }
+
+      this.increasePositionThumb(x, thumbLeft);
+      this.reducePositionThumb(x, thumbLeft);
+    }
+  }
+
+  public increasePositionThumb(x: number, thumbLeft: number) {
+    if (this.interval) {
+      if ( x >= (thumbLeft + this.interval / 2 ) ) {
+        let symbol: string = '';
+        if (this.thumb.hasClass('slider__thumb_vertical')) {
+          symbol = '-';
+        } else {
+          symbol = '+';
+        }
+        let i = x - thumbLeft;
+        let counter = 0;
+        while (i > 0) {
+          if (i - this.interval >= 0) {
+            i -= this.interval;
+            counter += 1;
+          } else if (i - this.interval / 2 >= 0) {
+            i -= this.interval / 2;
+            counter += 1;
+          } else {
+            i = 0;
+          }
+        }
+        this.notifySubscribers('moveThumb', {symbol, counter, index: this.index});
+      }
+    }
+  }
+
+  public reducePositionThumb(x: number, thumbLeft: number) {
+    if (this.interval) {
+      if (x <= (thumbLeft - this.interval / 2 )) {
+        let symbol: string = '';
+        if (this.thumb.hasClass('slider__thumb_vertical')) {
+          symbol = '+';
+        } else {
+          symbol = '-';
+        }
+        let i = thumbLeft - x;
+        let counter = 0;
+        while (i > 0) {
+          if (i - this.interval >= 0) {
+            i -= this.interval;
+            counter += 1;
+          } else if (i - this.interval / 2 >= 0) {
+            i -= this.interval / 2;
+            counter += 1;
+          } else {
+            i = 0;
+          }
+        }
+        this.notifySubscribers('moveThumb', {symbol, counter, index: this.index});
+      }
+    }
+  }
+
+  public handleDocumentMouseUp() {
+    document.removeEventListener('mousemove', this.handleDocumentMouseMove);
+    document.removeEventListener('mouseup', this.handleDocumentMouseUp);
   }
 
   public getSymbol(): string {
