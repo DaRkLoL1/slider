2c165666ee52a4d1783fabb3619dd42f222dcced
Igor Frolov <darklol_777@mail.ru>
2020-01-29 23:07:37 +0300

    Тесты #4

--- test/plugin-test/model/model-test.ts
+++ test/plugin-test/model/model-test.ts
@@ -31,58 +31,6 @@ describe('Model', () => {
 
 });
 
-describe('model handle', () => {
-
-  let handle: ModelValues;
-
-  beforeEach(() => {
-     handle = new ModelValues(0);
-  });
-
-  it('создать ручку и получить значение', () => {
-    const handle: ModelValues = new ModelValues(50);
-    const value: number = handle.getValue();
-    expect(value).toEqual(50);
-  });
-
-  it('увеличить значение на step', () => {
-    handle.increaseValue({max: 100, step: 1, counter: 1});
-    expect(handle.getValue()).toEqual(1);
-  });
-
-  it('увеличить значение на 110', () => {
-    handle.increaseValue({max: 100, step: 110, counter: 1});
-    expect(handle.getValue()).toEqual(100);
-  });
-
-  it('уменьшить значение на 10', () => {
-    handle.setValue({value: 40, min: 0, max: 100});
-    handle.reduceValue({min: 0, step: 10, counter: 1});
-    expect(handle.getValue()).toEqual(30);
-  });
-
-  it('уменьшить значение на 10', () => {
-    handle.reduceValue({min: 0, step: 10, counter: 1});
-    expect(handle.getValue()).toEqual(0);
-  });
-
-  it('изменить значение ручки больше мах значения', () => {
-    handle.setValue({value: 200, min: 0, max: 100});
-    expect(handle.getValue()).toEqual(100);
-  });
-
-  it('изменить значение ручки меньше мin значения', () => {
-    handle.setValue({value: -200, min: 0, max: 100});
-    expect(handle.getValue()).toEqual(0);
-  });
-
-  it('изменить значение ручки', () => {
-    handle.setValue({value: 50, min: 0, max: 100});
-    expect(handle.getValue()).toEqual(50);
-  });
-
-});
-
 describe('изменить значение из Model',  () => {
 
   it('изменить значение из Model',  () => {
