29f4655484bc3ac461f5827b8ca567a392096557
Igor Frolov <darklol_777@mail.ru>
2020-01-30 18:43:49 +0300

    Тесты #4

--- test/plugin-test/view/view-thumb-test.ts
+++ test/plugin-test/view/view-thumb-test.ts
@@ -4,69 +4,53 @@ const css = (window as any).__FIXTURES__['test/style.css'];
 
 describe('ViewThumb', () => {
   let thumb: ViewThumb;
-
+  let count: string;
   beforeEach(() => {
     setFixtures('<div class="slider__thumb"></div>');
     setStyleFixtures(css);
     thumb = new ViewThumb($('.slider__thumb'), 0, 0);
     thumb.installValue(250, 5);
-    thumb.addSubscribers('moveThumb', () => {
-      return true;
+    thumb.addSubscribers('moveThumb', (options) => {
+      count = options.symbolMinusOrPlus;
     });
   });
 
   it('отменить dragstart', () => {
-    thumb.handleThumbDragStart();
+    const isTrue: boolean = thumb.handleThumbDragStart();
+    expect(isTrue).toBeFalsy();
   });
 
   it('увеличить значение', () => {
     thumb.increasePositionThumb(100, 20);
+    expect(count).toEqual('+');
   });
 
   it('уменьшить значение', () => {
     thumb.reducePositionThumb(10, 200);
-  });
-
-  it('вызвать handleMouseDown', () => {
-    const event = new MouseEvent('mousemove', {
-      bubbles: true,
-      cancelable: true,
-      clientX: 100,
-      clientY: 100,
-    });
-    thumb.handleDocumentMouseMove(event);
+    expect(count).toEqual('-');
   });
 });
 
 describe('ViewThumb вертикальный вид', () => {
   let thumb: ViewThumb;
-
+  let count: string;
   beforeEach(() => {
     setFixtures('<div class="slider__thumb slider__thumb_vertical"></div>');
     setStyleFixtures(css);
     thumb = new ViewThumb($('.slider__thumb'), 0, 0);
     thumb.installValue(250, 5);
-    thumb.addSubscribers('moveThumb', () => {
-      return true;
+    thumb.addSubscribers('moveThumb', (options) => {
+      count = options.symbolMinusOrPlus;
     });
   });
 
   it('увеличить значение', () => {
     thumb.increasePositionThumb(100, 20);
+    expect(count).toEqual('-');
   });
 
   it('уменьшить значение', () => {
     thumb.reducePositionThumb(10, 200);
-  });
-
-  it('вызвать handleMouseDown', () => {
-    const event = new MouseEvent('mousemove', {
-      bubbles: true,
-      cancelable: true,
-      clientX: 100,
-      clientY: 100,
-    });
-
-    thumb.handleDocumentMouseMove(event);
+    expect(count).toEqual('+');
   });
 });
